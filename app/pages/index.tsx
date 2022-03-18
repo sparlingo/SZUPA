de8c3bd02acf2a5496287fde16593cf875eff255
Kevin Sparling <kevinsparling@gmail.com>
2022-03-18 12:08:12 +0800

    first commit

---
+++ app/pages/index.tsx
@@ -0,0 +1,64 @@
+import { Suspense } from "react"
+import { Image, Link, BlitzPage, useMutation, Routes } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+import logout from "app/auth/mutations/logout"
+import { Container, Heading } from "@chakra-ui/react"
+
+const UserInfo = () => {
+  const currentUser = useCurrentUser()
+  const [logoutMutation] = useMutation(logout)
+
+  if (currentUser) {
+    return (
+      <>
+        <button
+          className="button small"
+          onClick={async () => {
+            await logoutMutation()
+          }}
+        >
+          Logout
+        </button>
+        <div>
+          User id: <code>{currentUser.id}</code>
+          <br />
+          User role: <code>{currentUser.role}</code>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Link href={Routes.SignupPage()}>
+          <a className="button small">
+            <strong>Sign Up</strong>
+          </a>
+        </Link>
+        <Link href={Routes.LoginPage()}>
+          <a className="button small">
+            <strong>Login</strong>
+          </a>
+        </Link>
+      </>
+    )
+  }
+}
+
+const Home: BlitzPage = () => {
+  return (
+    <Container>
+      <main>
+        <Heading as="h1" size="2xl">
+          SZUPA
+        </Heading>
+      </main>
+    </Container>
+  )
+}
+
+Home.suppressFirstRenderFlicker = true
+Home.getLayout = (page) => <Layout title="Home">{page}</Layout>
+//Home.authenticate = true
+
+export default Home
